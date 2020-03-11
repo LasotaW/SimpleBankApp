2c8ae7606cf5fd0de6d900197e34b57f2b3d5fd7
Lasota <wiktor.lasota.wt@onet.pl>
2020-03-11 15:13:05 +0100

    Added status and contacttype

--- system/admin.py
+++ system/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 from .models import Person
+#from .models import ContactType
 
 admin.site.register(Person)
+#admin.site.register(ContactType)
--- system/models.py
+++ system/models.py
@@ -7,5 +7,15 @@ class Person(models.Model):
     Nazwisko = models.CharField(max_length=50)
     join_date = models.DateField('Data dołączenia', default=datetime.date.today)
 
-class ContactType(models.Model):
-    znajomy = models.ForeignKey('Person', on_delete=models.CASCADE)
+    def __str__(self):
+        return 'Klient: ' + self.Imię + ' ' + self.Nazwisko
+
+    contactType = models.TextChoices('Typ kontaktu','ZNAJOMY RODZINA INTERNET')
+    TypKontaktu = models.CharField(blank=True, choices=contactType.choices, max_length=50)
+
+    stat = models.TextChoices('Status klienta','NOWY ZAINTERESOWANY NIEZAINTERESOWANY ZAWIESZONY')
+    Status = models.CharField(blank=True, choices=stat.choices, max_length=50)
+
+#class ContactType(models.Model):
+   # Osoba = models.ForeignKey(Person, on_delete=models.CASCADE)
+    
